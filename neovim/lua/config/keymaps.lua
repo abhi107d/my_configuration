17a177c1ac491b9dd626875c9fba4635cf608293
abhi107d <abhishek107d@gmail.com>
2024-03-29 03:18:40 -0400

    move line up/down

--- neovim/lua/config/keymaps.lua
+++ neovim/lua/config/keymaps.lua
@@ -3,6 +3,13 @@ local keymap = vim.keymap
 keymap.set("n","<leader>m", ":NvimTreeFocus<CR>",{noremap =true,silent = true}) 
 keymap.set("n","<leader>e", ":NvimTreeToggle<CR>",{noremap =true,silent = true})
 
+--move line up or down
+vim.api.nvim_set_keymap('n', 'J', ':m .+1<CR>==', {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', 'K', ':m .-2<CR>==', {silent = true, noremap = true})
+vim.api.nvim_set_keymap('v', 'J', ":m '>+1<CR>gv=gv", {silent = true, noremap = true})
+vim.api.nvim_set_keymap('v', 'K', ":m '<-2<CR>gv=gv", {silent = true, noremap = true})
+
+--move line to left or right
 vim.keymap.set("v", "<", "<gv", { silent = true, noremap = true })
 vim.keymap.set("v", ">", ">gv", { silent = true, noremap = true })
 
